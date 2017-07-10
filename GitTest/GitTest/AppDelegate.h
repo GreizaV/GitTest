572579cdeee33e81732596f9b05bf2f1dddcebe0
geyuze <geyuze@bytedance.com>
2017-07-10 18:02:06 +0800

    New Projects

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by geyuze@bytedance.com on 2017/7/10.
+//  Copyright © 2017年 geyuze@bytedance.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by geyuze@bytedance.com on 2017/7/10.
+//  Copyright © 2017年 geyuze@bytedance.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
