092d85bd2b336dc1e387e05ba66a45c1fafc27b6
geyuze <geyuze@bytedance.com>
2017-10-23 11:04:39 +0800

    tableviewController

---
+++ GitTest/GitTest/GTTableViewModel.h
@@ -0,0 +1,13 @@
+//
+//  GTTableViewModel.h
+//  GitTest
+//
+//  Created by geyuze@bytedance.com on 2017/10/23.
+//  Copyright © 2017年 geyuze@bytedance.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTableViewModel : NSObject
+
+@end
---
+++ GitTest/GitTest/GTTableViewModel.m
@@ -0,0 +1,13 @@
+//
+//  GTTableViewModel.m
+//  GitTest
+//
+//  Created by geyuze@bytedance.com on 2017/10/23.
+//  Copyright © 2017年 geyuze@bytedance.com. All rights reserved.
+//
+
+#import "GTTableViewModel.h"
+
+@implementation GTTableViewModel
+
+@end
